e341c0ed39ca8b226ebf674408044c4e08534899
goose-in-grass <136070190+goose-in-grass@users.noreply.github.com>
2024-07-01 18:05:39 +0300

    Создали функцию подсчета стоимости

--- lesson_1.py
+++ lesson_1.py
@@ -1,7 +1,10 @@
 # создание класса
 
 class Item:
-    pass  # пропуск, чтобы не получать ошибок
+    def calculate_toatal_price(self, x, y):
+        return x * y
+
+
 
 
 
@@ -10,4 +13,10 @@ item1 = Item()  #объект класса
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate_toatal_price(item1.price, item1.quantity))
 
+item2 = Item()  #объект класса
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
+print(item2.calculate_toatal_price(item2.price, item2.quantity))
