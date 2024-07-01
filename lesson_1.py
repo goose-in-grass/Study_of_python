2eee3a5036d0032262cf6a069e0f433eed80d1ef
goose-in-grass <136070190+goose-in-grass@users.noreply.github.com>
2024-07-01 17:56:34 +0300

    Создали объект класса, узнал про "pass"

---
+++ lesson_1.py
@@ -0,0 +1,13 @@
+# создание класса
+
+class Item:
+    pass  # пропуск, чтобы не получать ошибок
+
+
+
+
+item1 = Item()  #объект класса
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+
