bacdb2d048463ee5ab70dfd46abb902c019572f5
goose-in-grass <136070190+goose-in-grass@users.noreply.github.com>
2024-07-01 21:19:06 +0300

    Что-то вроде уровня доступа, на примере скидки. Для первого товара скидка составила 20%, а для второго 30%. В первом случае, объект 1 не имеет собственного понятия pay_rate, поэтому он поднимается на уровень класса и использует pay_rate = 0.8. Во-втором же случаи, мы сами определили для объекта 2 pay_rate = 0.7, путем замены конструкции "self.price = self.price * Item.pay_rate" на "self.price = self.price * self.pay_rate"

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,7 @@
 # создание класса
 
 class Item:
+    pay_rate = 0.8  # after 20% discount
 
     def __init__(self, name: str, price: float, quantity=0):
         # присвоение
@@ -19,12 +20,18 @@ class Item:
 
     def calculate_total_price(self):
         return self.price * self.quantity
+
     # не передаем параметры в def потому что self уже содержит их (price & quantity)
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
 
 item1 = Item("Phone", 100, 1)  # объект класса
-item2 = Item("Laptop", 1000, 1)  # объект класса
-item2.has_numpad = False
+item1.apply_discount()
+print(item1.price)
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+item2 = Item("Laptop", 1000, 3)
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
