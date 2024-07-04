03eb800e5876cbbb7291c7b156c1dcc0bc021d7b
goose-in-grass <136070190+goose-in-grass@users.noreply.github.com>
2024-07-04 14:06:28 +0300

    возвращаем список товаров массивом. Новая функция __repr__. Теперь вывожу товары не по элемнтам, а одним списком, благодаря:      def __repr__(self):         return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
    item1 = Item("Phone", 100, 1)
    item2 = Item("Laptop", 1000, 3)
    item3 = Item("Cable", 10, 5)
    item4 = Item("Mouse", 50, 5)
    item5 = Item("Keyboard", 75, 5)

--- lesson_1.py
+++ lesson_1.py
@@ -3,16 +3,21 @@
 class Item:
     pay_rate = 0.8  # after 20% discount
 
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
+        # проверка
+        assert price >= 0, f"Долбаеб, {price} цена не может быть отрицательной"
+        assert quantity >= 0, f"Децел, {quantity} количество тоже"
+        # assert - ключевое слово для проверки ваших ожиданий
+
         # присвоение
         self.name = name
         self.price = price
         self.quantity = quantity
 
-        # проверка
-        assert price >= 0, f"Долбаеб, {price} цена не может быть отрицательной"
-        assert quantity >= 0, f"Децел, {quantity} количество тоже"
-        # assert - ключевое слово для проверки ваших ожиданий
+
+        #
+        Item.all.append(self)
 
     # name:str - Это знак ввода, проверка на правильный ввод типа данных
 
@@ -27,11 +32,16 @@ class Item:
         self.price = self.price * self.pay_rate
 
 
-item1 = Item("Phone", 100, 1)  # объект класса
-item1.apply_discount()
-print(item1.price)
+#TODO: разобраться с функцией __repr__ and self
 
+
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
+
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
