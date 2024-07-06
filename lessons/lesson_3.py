7a61b3caeb369a5103d5c73b97cfac6c25304ebf
goose-in-grass <136070190+goose-in-grass@users.noreply.github.com>
2024-07-07 02:57:56 +0300

    добавили маг. метод __del__ и его описание

--- lessons/lesson_3.py
+++ lessons/lesson_3.py
@@ -5,10 +5,13 @@ class Point:
     color = "red"
     circle = 2
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         print("вызов")
         self.x = x
         self.y = y
+
+    def __del__(self):
+        print("удаление экземпляра" + str(self))
 
     def Set_coords(self, x, y):
         self.x = x
@@ -20,3 +23,5 @@ class Point:
 
 pt = Point(2, 3)
 print(pt.__dict__)
+
+# пока на объект есть хотя бы одна внешняя ссылка, то он не удаляется
